ed5d32331208285c64692a8e5a6723cdac3194d3
paul albrecht <paulalbrecht@oakland.edu>
2019-02-08 21:47:11 -0500

    Beer song
    in the java.java file the main class now calls the  Ninety_Nine_Bottles_of_Beer() function. The
    Ninety_Nine_Bottles_of_Beer() function now prints out the beer song

--- BeerSong/src/beersong/java.java
+++ BeerSong/src/beersong/java.java
@@ -14,16 +14,30 @@ public class java {
     /**
      * @param args the command line arguments
      */
-    public static void main() {
+    public static void main(String[] args) {
         // TODO code application logic here
-        
+        Ninety_Nine_Bottles_of_Beer();
         
     }
     
     public static void 	Ninety_Nine_Bottles_of_Beer() {
         // TODO code application logic here
+        System.out.println("Starting the beer song");
+        int beer = 99;
+        
+        while (beer >0){
+        System.out.println(beer + " bottles of beer on the wall");
+        System.out.println(beer + " bottles of beer.");
+        System.out.println("Take one down.");
+        System.out.println("Pass it around.");
+        beer --;
         
         
+        
+        
+        }
+        
+        System.out.println("No more bottles of beer on the wall :(");
     }
     
 }
